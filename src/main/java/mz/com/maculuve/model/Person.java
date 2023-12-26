963c010585aa5db8126ad2345b471efe0f77afb8
paulo-maculuve <paulomaculuve758@gmail.com>
2023-12-26 13:24:46 +0200

    Usando JPA na entidade Person

--- src/main/java/mz/com/maculuve/model/Person.java
+++ src/main/java/mz/com/maculuve/model/Person.java
@@ -3,13 +3,27 @@ package mz.com.maculuve.model;
 import java.io.Serializable;
 import java.util.Objects;
 
-public class Person implements Serializable {
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "person")
+public class Person implements Serializable {
 	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@Column(name= "first_name", nullable = false, length = 80)
 	private String firstName;
+	@Column(name = "last_name", nullable = false, length = 80)
 	private String lastName;
+	@Column(nullable = false, length = 90)
 	private String address;
+	@Column(nullable = false, length = 10)
 	private String gender;
 
 	public Person() {
