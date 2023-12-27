4eeba5d1395be9ebef72c889f0a9975c39e6f6fc
paulo-maculuve <paulomaculuve758@gmail.com>
2023-12-27 13:30:03 +0200

    JSON Serialization

--- src/main/java/mz/com/maculuve/data/vo/v1/PersonVO.java
+++ src/main/java/mz/com/maculuve/data/vo/v1/PersonVO.java
@@ -3,12 +3,21 @@ package mz.com.maculuve.data.vo.v1;
 import java.io.Serializable;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonPropertyOrder({"id", "first_name", "last_name", "address", "gender"})
 public class PersonVO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private Long id;
+	
+	@JsonProperty("first_name")
 	private String firstName;
+	
+	@JsonProperty("last_name")
 	private String lastName;
 	private String address;
+//	@JsonIgnore // Ignora a serializacao JSON
 	private String gender;
 
 	public PersonVO() {
