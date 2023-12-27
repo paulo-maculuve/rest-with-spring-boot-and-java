a4f021746f8ce282bf79cf726dc85a3b117b6aac
paulo-maculuve <paulomaculuve758@gmail.com>
2023-12-27 14:14:50 +0200

    ContentNegotiationConfigurer via header

--- src/main/java/mz/com/maculuve/conf/WebConfig.java
+++ src/main/java/mz/com/maculuve/conf/WebConfig.java
@@ -9,9 +9,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-		configurer.favorParameter(true).parameterName("mediaType").ignoreAcceptHeader(true)
-				.useRegisteredExtensionsOnly(false).defaultContentType(MediaType.APPLICATION_JSON)
-				.mediaType("json", MediaType.APPLICATION_JSON).mediaType("xml", MediaType.APPLICATION_XML);
+
+		// Via Query Parameter
+//		configurer.favorParameter(true).parameterName("mediaType").ignoreAcceptHeader(true)
+//				.useRegisteredExtensionsOnly(false).defaultContentType(MediaType.APPLICATION_JSON)
+//				.mediaType("json", MediaType.APPLICATION_JSON).mediaType("xml", MediaType.APPLICATION_XML);
+
+		// Via Header Parameter
+		configurer.favorParameter(false).ignoreAcceptHeader(false).useRegisteredExtensionsOnly(false)
+				.defaultContentType(MediaType.APPLICATION_JSON).mediaType("json", MediaType.APPLICATION_JSON)
+				.mediaType("xml", MediaType.APPLICATION_XML);
 	}
 
 }
